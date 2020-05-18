e110fb88c65de4f9d55136c7f8913a618fe2bd26
nastia\Настя <nastiasamborska90@gmail.com>
2020-05-18 16:54:08 +0300

    task1

--- src/com/nastia/Arrays/Task1.java
+++ src/com/nastia/Arrays/Task1.java
@@ -2,9 +2,9 @@ package com.nastia.Arrays;
 
 public class Task1 {
     public static void main(String[] args) {
-        String [] array1 = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
-        for (int k = 0; k < array1.length; k++) {
-            System.out.println(array1[k]);
+        String [] Months = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+        for (String i: Months) {
+            System.out.println(i);
         }
     }
 }
